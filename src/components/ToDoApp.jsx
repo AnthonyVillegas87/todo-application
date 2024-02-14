b4a248c61d614e030227682b8171db08e3f513b1
Anthony Villegas <villegasanthony87@yahoo.com>
2024-02-14 11:18:53 -0600

    Set up Login component w/ form and hardcoded authentication of user. Installed Router to redirect to welcome page upon successful submission

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,33 +1,78 @@
-
+import './ToDoApp.css'
+import {useState} from "react";
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
 export default function ToDoApp() {
     return (
         <div className="ToDoApp">
-            To-Do Management Application
-            <LoginComponent/>
-            {/*<WelcomeComponent/>*/}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginComponent/>}></Route>
+                    <Route path='/login' element={<LoginComponent/>}></Route>
+                    <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
 
 function LoginComponent() {
+
+    const [username, setUsername] = useState('username')
+    const [password, setPassword] = useState('')
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+    const navigate = useNavigate()
+
+
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+
+    function handleSubmit() {
+        if(username === 'George Tudor' && password === 'mypassword') {
+            console.log('Success')
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+            navigate('/welcome')
+        } else {
+            console.log('Failed')
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
+
     return (
         <div className="Login">
+
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage">Authentication Failed. Please check your credentials.</div>}
+
             <div className="LoginForm">
                 <div>
-                    <label>User Name</label>
-                    <input type="text" name="username"/>
+                    <label>User Name: </label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
-                    <label>Password</label>
-                    <input type="password" name="password"/>
+                    <label>Password: </label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
                 </div>
             </div>
         </div>
     )
 }
+
+
+
 
 function WelcomeComponent() {
     return (
