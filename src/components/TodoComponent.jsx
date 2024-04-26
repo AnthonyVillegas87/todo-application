e799ff998d69b344c810577c9388a827f3b2e411
Anthony Villegas <villegasanthony87@yahoo.com>
2024-04-26 13:40:41 -0500

    Installed Formik and Moment. Formik to handle details when updating each Todo. Successful via console

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -2,12 +2,14 @@ import {isRouteErrorResponse, useParams} from 'react-router-dom';
 import {retrieveTodoApi} from "../api/ApiService";
 import {useAuth} from "../security/AuthenticationContext";
 import {useEffect, useState} from "react";
+import {Field, Form, Formik} from "formik";
 
 
 export default function TodoComponent(){
     const {id} = useParams()
 
     const [description, setDescription] = useState('')
+    const [targetDate, setTargetDate] = useState('')
 
     const authContext = useAuth()
 
@@ -22,10 +24,14 @@ export default function TodoComponent(){
         retrieveTodoApi(username, id)
             .then(response => {
                setDescription(response.data.description)
-
+                setTargetDate(response.data.targetDate)
             })
             .catch(error => console.log(error))
 
+    }
+
+    function onSubmit(values) {
+        console.log(values)
     }
 
 
@@ -33,7 +39,31 @@ export default function TodoComponent(){
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                description : {description}
+
+                <Formik initialValues={ { description, targetDate } }
+                   enableReinitialize={true}
+                        onSubmit={onSubmit}
+                >
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description:</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset>
+
+                                <fieldset className="form-group">
+                                    <label>Target Date:</label>
+                                    <Field type="date" className="form-control" name="targetDate"/>
+                                </fieldset>
+
+                                <div>
+                                    <button className="btn btn-success m-5" type="submit">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
             </div>
         </div>
 
